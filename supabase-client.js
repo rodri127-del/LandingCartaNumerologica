d583f024caa90e80b2dda34f4b2d385f69c47230
MiBot50K <rodri127@gmail.com>
2025-10-19 12:50:38 +0200

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,7 +1,7 @@
 // supabase-client.js
 // SUSTITUYE ESTOS VALORES POR LOS DE TU PROYECTO EN SUPABASE
 const SUPABASE_URL = 'https://cnzldhhpwwwssaraxrcf.supabase.co';
-const SUPABASE_ANON_KEY = SUPABASE_ANON_KEY;
+const SUPABASE_ANON_KEY = 'SUPABASE_ANON_KEY';
 
 // Inicializa el cliente de Supabase y lo hace disponible globalmente
 window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
